42608f6be6bccd6531327e2a471ac9d455ce0cb2
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2021-09-17 17:25:16 +0700

    feat: connect to mysql

--- go.mod
+++ go.mod
@@ -6,8 +6,11 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/echo/v4 v4.5.0 // indirect
 	github.com/labstack/gommon v0.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20210915214749-c084706c2272 // indirect
+	gorm.io/driver/mysql v1.1.2 // indirect
+	gorm.io/gorm v1.21.15 // indirect
 )
--- main.go
+++ main.go
@@ -3,15 +3,51 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+// database config
+var (
+	DB *gorm.DB
+)
+
+func InitDB() {
+	dsn := "root:@/pinjem?charset=utf8&parseTime=True&loc=Local"
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	Migration()
+}
+
 type User struct {
-	Name     string
-	Email    string
-	Password string
+	ID          uint   `gorm:"primary_key"`
+	Email       string `gorm:"unique_index"`
+	Password    string `gorm:"not null"`
+	Fullname    string `gorm:"not null"`
+	NIK         string `gorm:"not null"`
+	PhoneNumber string `gorm:"not null"`
+	Birthdate   string `gorm:"not null"`
+	Address     string `gorm:"not null"`
+	Provinsi    string `gorm:"not null"`
+	Kota        string `gorm:"not null"`
+	Kecamatan   string `gorm:"not null"`
+	Kelurahan   string `gorm:"not null"`
+	PostalCode  string `gorm:"not null"`
+	Role        string `gorm:"not null"`
+	Status      int    `gorm:"not null"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+func Migration() {
+	DB.AutoMigrate(&User{})
 }
 
 type Response struct {
@@ -22,28 +58,22 @@ type Response struct {
 }
 
 func GetUser(c echo.Context) error {
-	user := User{Name: "Alterra", Email: "alterra@alterra.id"}
+	user := User{Fullname: "Alterra", Email: "alterra@alterra.id"}
 	return c.JSON(http.StatusOK, user)
 }
 
 func PostRegisterController(c echo.Context) error {
-	name := c.FormValue("name")
-	email := c.FormValue("email")
-	password := c.FormValue("password")
-
-	var response Response
 	var user User
+	var response Response
+	c.Bind(&user)
 
-	if name == "" || email == "" || password == "" {
+	if user.Fullname == "" || user.Email == "" || user.Password == "" {
 		response.Status = http.StatusBadRequest
 		response.Success = false
 		response.Message = "Please fill all the fields"
 		response.Content = ""
 	} else {
-		user.Name = name
-		user.Email = email
-		user.Password = password
-
+		DB.Where(&User{Email: user.Email}).FirstOrCreate(&user)
 		response.Status = http.StatusOK
 		response.Success = true
 		response.Message = "Register success"
@@ -54,6 +84,7 @@ func PostRegisterController(c echo.Context) error {
 }
 
 func main() {
+	InitDB()
 	e := echo.New()
 	e.GET("/api/v1/user", GetUser)
 	e.POST("/api/v1/register", PostRegisterController)
